f15c13c40a16b0531212eb0180e52822a2626e66
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-02-18 11:37:53 -0500

    manually creating dataloader in controller constructor

--- src/main/java/com/github/bcdxn/sibdep/graphql/PersonController.java
+++ src/main/java/com/github/bcdxn/sibdep/graphql/PersonController.java
@@ -1,15 +1,37 @@
 package com.github.bcdxn.sibdep.graphql;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+import org.dataloader.DataLoader;
 import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.execution.BatchLoaderRegistry;
 import org.springframework.stereotype.Controller;
 
+import reactor.core.publisher.Flux;
+
 @Controller
-record PersonController(PersonService personService) {
+class PersonController {
+
+  PersonService personService;
+
+  PersonController(
+    PersonService personService,
+    BatchLoaderRegistry registry
+  ) {
+    this.personService = personService;
+
+    // register batch loader
+    registry
+      .forTypePair(Integer.class, Person.class)
+      .registerBatchLoader(
+        (integers, batchLoaderEnvironment) -> Flux.fromIterable(personService.getPeopleById(integers))
+      );
+  }
+
   @QueryMapping
   List<Person> people() {
     return personService.getAllPeople();
@@ -20,12 +42,17 @@ record PersonController(PersonService personService) {
   //   return personService.getPersonById(person.bestFriendId());
   // }
 
-  @BatchMapping
-  List<Person> bestFriend(List<Person> people) {
-    List<Integer> ids = people
-      .stream()
-      .map(p -> p.bestFriendId())
-      .collect(Collectors.toList());
-    return personService.getPeopleById(ids);
+  // @BatchMapping
+  // List<Person> bestFriend(List<Person> people) {
+  //   List<Integer> ids = people
+  //     .stream()
+  //     .map(p -> p.bestFriendId())
+  //     .collect(Collectors.toList());
+  //   return personService.getPeopleById(ids);
+  // }
+
+  @SchemaMapping
+  CompletableFuture<Person> bestFriend(Person person, DataLoader<Integer, Person> dataLoader) {
+    return dataLoader.load(person.bestFriendId());
   }
 }
